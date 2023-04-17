32d48e4fad19c8ee94dd42dddc8537d84b935f8b
高橋 輝 H.T <takahashi@saice.net>
2023-04-17 19:02:58 +0900

    Add Page and Link

--- myapp.rb
+++ myapp.rb
@@ -2,9 +2,13 @@ require 'sinatra'
 require 'sinatra/reloader'
  
 get '/' do
-    "<h1>Hello World!</h1><p>asdfghjk</p><a href='/test'>test</a>"
+    "<h1>Hello World!</h1><p>links</p><a href='/test'>test</a><br><a href='/test_jpn'>test_jpn</a>"
 end
 
 get '/test' do
     "<h2>Hello Worldtest!</h2><p>asdfghjk</p>"
+end
+
+get '/test_jpn' do
+    "<h2>こんにちは!</h2><p>追加したテストページです</p>"
 end
